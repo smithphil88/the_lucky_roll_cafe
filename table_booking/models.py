72c9c83b22e61d91b233fbcb90236467340682f7
Phil Smith <smithphil868@gmail.com>
2024-12-03 14:18:08 +0000

    create a superuser for the admin panel and create a booking model

--- table_booking/models.py
+++ table_booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Booking(models.Model):
+    num_of_guests = models.PositiveIntegerField()
+    booked_on = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    additional_message = models.TextField()
